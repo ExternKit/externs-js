0a938c2a63c5405f43533a71b65285e9066a8639
Fabien Antoine <fab.antoine@gmail.com>
2016-03-10 17:00:03 +0100

    Squashed 'extern/' changes from 03b95ad..073ae22
    073ae22 Use Rest generic parameters for Callback and Either
    6d104be Updated js.extern.Extern management to allow custom name depending on access mode through @:externjs Moved @:namespace option in @:externjs(namespace=true)
    
    git-subtree-dir: extern
    git-subtree-split: 073ae22b529fef522c6a16833e8b3b16a849d807

--- src/js/extern/Callback.hx
+++ src/js/extern/Callback.hx
@@ -1,11 +1,4 @@
 package js.extern;
 
-typedef Callback0 = Null<Error> -> Void;
-
-typedef Callback1<A> = Null<Error> -> Null<A> -> Void;
-
-typedef Callback2<A, B> = Null<Error> -> Null<A> -> Null<B> -> Void;
-
-typedef Callback3<A, B, C> = Null<Error> -> Null<A> -> Null<B> -> Null<C> -> Void;
-
-typedef Callback<T> = Callback1<T>;
+@:genericBuild(js.extern.CallbackBuilder.build())
+class Callback<Rest> {}
