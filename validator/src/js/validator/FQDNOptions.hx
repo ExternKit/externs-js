0f8c46d6df0caa74cae5da6e5a03cac58fb88ef1
Fabien Antoine <fab.antoine@gmail.com>
2016-03-24 11:52:50 +0100

    Added validator code

---
+++ validator/src/js/validator/FQDNOptions.hx
@@ -0,0 +1,7 @@
+package js.validator;
+
+typedef FQDNOptions = {
+    ?require_tld : Bool,
+    ?allow_underscores : Bool,
+    ?allow_trailing_dot : Bool
+}
---
+++ validator/src/js/validator/NormalizeEmailOptions.hx
@@ -0,0 +1,7 @@
+package js.validator;
+
+typedef NormalizeEmailOptions = {
+    ?lowercase : Bool,
+    ?remove_dots : Bool,
+    .remove_extension : Bool
+}
