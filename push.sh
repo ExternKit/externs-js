5b22595d223aa88c12d774bb16b24f1bec59a503
Fabien Antoine <fab.antoine@gmail.com>
2016-05-04 09:57:18 +0200

    Fixed echo

--- push.sh
+++ push.sh
@@ -6,6 +6,6 @@ subtrees=($(git log | grep git-subtree-dir | tr -d ' ' | cut -d ":" -f2 | sort |
 # push each subtree
 for tree in "${subtrees[@]}"; do
     [[ "$tree" == "extern" ]] && repository="extern-js" || repository="extern-js-$tree"
-    echo "$repository"
+    echo "---"
     git subtree push -P "$tree" "git@github.com:ExternKit/$repository.git" master
 done
